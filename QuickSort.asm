b69089f4308185a22526601c3a9202df75066c0f
Trương Nhật Duy <48389329+zeref278@users.noreply.github.com>
2020-07-03 18:34:23 +0700

    Add files via upload

--- QuickSort.asm
+++ QuickSort.asm
@@ -1,19 +1,17 @@
-#STRINGS
+
 .data
 fileNameInput: .asciiz "input_sort.txt"
-fileNameOutput: .asciiz "testout.txt"
-fileWords: .space 1024
+fileNameOutput: .asciiz "output_sort.txt"
+fileWords: .space 16384
 characterSpace: .asciiz " "
+characterTab: .asciiz "\t"
 
 array:  .word 0:1000
-strNl: .asciiz "\t"
-number: .word 8
 
 .text
 	.globl main
 	
 main:
-
 
 	#Open file to read
 	
@@ -21,13 +19,13 @@ main:
     	la $a0,fileNameInput
     	li $a1,0
     	syscall
-    	move $s0,$v0        	# save the file descriptor. $s0 = file
+    	move $s0,$v0
 	
 	#Read file
 	li $v0, 14
 	move $a0,$s0
 	la $a1,fileWords
-	la $a2,1024
+	la $a2,16384
 	syscall
 
 	#Convert char* to int array
@@ -39,7 +37,6 @@ main:
     	syscall
 
 
-    	
 	la $t0, array # Moves the address of array into register $t0.
 	addi $a0, $t0, 0 #the array.
 	addi $a1, $0, 0 #(low = 0)
@@ -68,39 +65,25 @@ main:
 ###
 Swap:
 
-	addi $sp, $sp, -8	# Init stack
-
-	sw $ra,0($sp)		# store $ra
-	sw $t2,4($sp)
-
 	sll $t1, $a1, 2 	#t1 = 4a
 	add $t1, $a0, $t1	#t1 = array + 4a (address)
 	lw $s3, 0($t1)		#s3 = array[a]
 
-	sll $t2, $a2, 2		#t2 = 4b
-	add $t2, $a0, $t2	#t2 = arr + 4b (address)
-	lw $s4, 0($t2)		#s4 = arr[b]
+	sll $t9, $a2, 2		#t2 = 4b
+	add $t9, $a0, $t9	#t2 = arr + 4b (address)
+	lw $s4, 0($t9)		#s4 = arr[b]
 
 	sw $s4, 0($t1)		#swap
-	sw $s3, 0($t2)
-
- 	lw $ra, 0($sp)
-	lw $t2,4($sp)
-
- 	addi $sp, $sp, 8	#restore the stack
+	sw $s3, 0($t9)
 
  	jr $ra
-	
 
 
 Partition:
 
-	addi $sp, $sp, -16	#Init stack
+	addi $sp, $sp, -4	#Init stack
 
-	sw $a0, 0($sp)		#store a0
-	sw $a1, 4($sp)		#store a1
-	sw $a2, 8($sp)		#store a2
-	sw $ra, 12($sp)		#store return address
+	sw $ra, 0($sp)		#store return address
 	
 	move $s1, $a1		#s1 = low
 	move $s2, $a2		#s2 = high
@@ -113,16 +96,16 @@ Partition:
 	move $t4, $s1		#t4, j=low
 	addi $t5, $s2, -1	#t5 = high - 1
 
-	forloop: 
+	loopPartition: 
 		slt $t6, $t5, $t4	#t6=1 if j>high-1, t6=0 if j<=high-1
-		bne $t6, $0, endfor	#if t6=1 then branch to endfor
+		bne $t6, $0, endLoopPartition	#if t6=1 then branch to endfor
 
 		sll $t1, $t4, 2		#t1 = j*4
 		add $t1, $t1, $a0	#t1 = arr + 4j
 		lw $t7, 0($t1)		#t7 = arr[j]
 
 		slt $t8, $t2, $t7	#t8 = 1 if pivot < arr[j], t8=0 if arr[j]<=pivot
-		bne $t8, $0, endfif	#if t8=1 then branch to endfif
+		bne $t8, $0, endIfPartition	#if t8=1 then branch to endfif
 		addi $t3, $t3, 1	#i=i+1
 
 		move $a1, $t3		#a1 = i
@@ -131,23 +114,22 @@ Partition:
 		
 		addi $t4, $t4, 1	#j++
 
-		j forloop
+		j loopPartition
 
-	    endfif:
+	    endIfPartition:
 		addi $t4, $t4, 1	#j++
-		j forloop		#junp back to forloop
+		j loopPartition		#junp back to forloop
 
-	endfor:
+	endLoopPartition:
 		addi $a1, $t3, 1		#a1 = i+1
 		move $a2, $s2			#a2 = high
 				
 		jal Swap			#Swap arr[i+1] arr[high]
-		lw $a0, 0($sp)		
-		lw $a2, 8($sp)
-		lw $ra, 12($sp)
-		addi $sp, $sp, 16
-
+		
 		addi $v0, $t3, 1		#return i+1
+
+		lw $ra,0($sp)
+		add $sp,$sp,4
 
 		jr $ra
 
@@ -160,10 +142,10 @@ QuickSort:
 	sw $a2, 8($sp)			# high
 	sw $ra, 12($sp)			# return address
 
-	move $t0, $a2			#saving high in t0
 
-	slt $t1, $a1, $t0		# t1=1 if low < high, else 0
-	beq $t1, $0, endif		# if low >= high, endif
+	slt $t1, $a1, $a2		# t1=1 if low < high, else 0
+	beq $t1, $0, endifQS		# if low >= high, endif
+	
 
 	jal Partition			# call Partition 
 	move $s0, $v0			# index pivot
@@ -176,7 +158,7 @@ QuickSort:
 	lw $a2, 8($sp)			#a2 = high
 	jal QuickSort
 
- endif:
+ endifQS:
 
  	lw $a0, 0($sp)
  	lw $a1, 4($sp)
@@ -184,30 +166,7 @@ QuickSort:
  	lw $ra, 12($sp)
  	addi $sp, $sp, 16
  	jr $ra
-PrintArray:
-	li $t9, 0
-	lw $t8,0($sp)
-	addi $sp,$sp,4
-	sll $t8,$t8,2
-	Loop1:
 
-		beq $t9,$t8,ExitLoop1
-		
-		lw $t2, array($t9)
-
-		
-		add $v0,$0,1
-		add $a0,$0,$t2
-		syscall
-		addi $v0, $0, 4
-		la $a0, strNl
-		syscall
-		
-		addi $t9,$t9,4
-		
-		j Loop1
-	ExitLoop1:
-		jr $ra
 
 #######
 Parse:
@@ -286,15 +245,15 @@ Convert:
 
 	beq $a1,0,.endif1
 	whileLoop1:
-		#$a1=intterger
 		#$a0=address
-		#$a2=so luong phan tu-1
+		#$a1=intterger
+		#$a2=numberOfE-1
 		beqz $a1,endLoop1
 		
 		div $a1,$t1
 		mflo $a1
-		mfhi $t2 #so du
-		addi $t2,$t2,48 #chuyen ve ascii
+		mfhi $t2
+		addi $t2,$t2,48
 		sb $t2,0($a3)
 		addi $a3,$a3,-1
 		j whileLoop1
@@ -304,10 +263,10 @@ Convert:
 		addi $sp,$sp,4
 		jr $ra
 
-		.endif1:
-			li $t2,48
-			sb $t2,0($a3)
-			lw $ra,0($sp)
+	.endif1: #Num=0
+		li $t2,48
+		sb $t2,0($a3)
+		lw $ra,0($sp)
 		addi $sp,$sp,4
 		jr $ra
 			
@@ -320,7 +279,7 @@ WriteToFile:
     	move $s0,$v0
 
 	li $t9, 0
-	lw $t8,0($sp)
+	lw $t8,0($sp) #number of elements
 	addi $sp,$sp,4
 	sll $t8,$t8,2
 
